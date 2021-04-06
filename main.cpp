4a1cf6bb49d621ecf0395f19b981c31eb1184587
trejotrejotrejo <alextrejo10101@gmail.com>
2021-04-06 11:26:34 -0700

    Add user input

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <string>
 
-int main()
-{
+int main() {
 
   std::string name;
   std::cout << "What is your name?" << std::endl;
   std::cin >> name;
   std::cout << "Hello " << name << "!" << std::endl;
 
-  return 0;
-
+  return 1;
 }
