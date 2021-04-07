adaeb8257d6c1138364c283976ce2bbc9eaacf00
trejotrejotrejo <alextrejo10101@gmail.com>
2021-04-06 18:28:27 -0700

    Lab 2 Partner 2

--- test.cpp
+++ test.cpp
@@ -1,4 +1,5 @@
 #include "c-echo.h"
+#include "c-count.h"
 
 #include "gtest/gtest.h"
 
@@ -12,20 +13,20 @@ TEST(EchoTest, EmptyString) {
 	EXPECT_EQ("", echo(1, test_val));
 }
 
-TEST(EchoTest, OneLetter) {
-	char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "a";
-	EXPECT_EQ("a", echo(2, test_val));
-}
+//TEST(EchoTest, OneLetter) {
+//	char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "a";
+//	EXPECT_EQ("a", echo(2, test_val));
+//}
 
-TEST(EchoTest, HelloMyNameIs) {
-	char* test_val[6]; test_val[0] = "./c-echo"; test_val[1] = "hello"; test_val[2] = "my"; test_val[3] = "name"; test_val[4] = "is"; test_val[5] = "alex";
-	EXPECT_EQ("hello my name is alex", echo(6, test_val));
-}
+//TEST(EchoTest, HelloMyNameIs) {
+//	char* test_val[6]; test_val[0] = "./c-echo"; test_val[1] = "hello"; test_val[2] = "my"; test_val[3] = "name"; test_val[4] = "is"; test_val[5] = "alex";
+//	EXPECT_EQ("hello my name is alex", echo(6, test_val));
+//}
 
-TEST(EchoTest, Numbers) {
-	char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "0123456789";
-	EXPECT_EQ("0123456789", echo(2, test_val));
-} 
+//TEST(EchoTest, Numbers) {
+//	char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "0123456789";
+//	EXPECT_EQ("0123456789", echo(2, test_val));
+//} 
 
 TEST(CountTest, HelloWorld) {
 	std::string test_str = "hello world";
